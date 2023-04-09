012673f678e76974f4ab39eda3de38d283f5d7e5
Serhii Ostapenko <ostapkinpaskal@gmail.com>
2023-04-09 18:57:37 +0300

    correct props

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -7,7 +7,7 @@ const styleBgColor = {
 
 const Statistics = ({title, stats}) => {
 	return (<section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
+  {{title} ? (<h2 className={css.title}>{title}</h2>) : null}
 
   <ul className={css.statList}>
 		{stats.map(item => (<li key={item.id} className={css.item} style={styleBgColor}>
--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -26,12 +26,12 @@ const TransactionHistory = ({items}) => {
 };
 
 TransactionHistory.propTypes = {
-	transactions: PropTypes.arrayOf(
+  items: PropTypes.arrayOf(
 		PropTypes.exact({
-			id: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
 			type: PropTypes.string.isRequired,
-			amount: PropTypes.number.isRequired,
-			currency: PropTypes.string.isRequired
+			amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
 		})
 	),
 };
